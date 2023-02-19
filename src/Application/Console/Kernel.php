606b626a11fecbcb9be035307e19508ba004232b
Thales Bento <dev.tbarbosa.bento@gmail.com>
2023-02-19 15:12:59 -0300

    Command example

--- src/Application/Console/Kernel.php
+++ src/Application/Console/Kernel.php
@@ -9,6 +9,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        TestCommand::class,
+    ];
+
+    /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
